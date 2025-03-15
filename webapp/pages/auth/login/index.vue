d62db438288b341d4b426963ec2bdf4d299d69db
Semmelon <138894594+Semmelon@users.noreply.github.com>
2025-03-15 17:03:41 +0100

    add zod and add more to login

--- webapp/pages/auth/login/index.vue
+++ webapp/pages/auth/login/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession()
+    const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession()
+    const email = ref('')
+    const password = ref('')
+
+    async function login() {
+        await $fetch('/api/login', {
+            method: 'POST',
+            body: { email: email.value, password: password.value }
+        })
+        
+    }
 </script>
 
 <template>
@@ -9,9 +19,23 @@ const { loggedIn, user, session, fetch, clear, openInPopup } = useUserSession()
     <button @click="clear">Logout</button>
   </div>
   <div v-else>
+    <form @submit.prevent="login">
+        <label for="">Email: </label>
+        <input type="text" v-model="email">
+        <label for="">Password: </label>
+        <input type="password" v-model="password">
+        <button type="submit">Login</button>
+    </form>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <h1>Not logged in</h1>
-    <a href="/auth/google">Login with Google</a>
+    <a href="/auth/github">Login with GitHub</a>
     <!-- or open the OAuth route in a popup -->
-    <button @click="openInPopup('/auth/google')">Login with Google</button>
+    <button @click="openInPopup('/auth/github')">Login with GitHub</button>
   </div>
 </template>
--- webapp/server/api/login.post.ts
+++ webapp/server/api/login.post.ts
@@ -1,9 +1,9 @@
 export default defineEventHandler(async (event) => {
-    const { password } = await readBody(event)
+    const { email, password } = await readBody(event)
 
-    //Todo: also get username -> search in db for user -> if not exists -> throw error -> if exists has pw and compare hased user pw + given new pw -> if pw ewuals -> create user session
+    //Todo: also get email -> search in db for user -> if not exists -> throw error -> if exists has pw and compare hased user pw + given new pw -> if pw ewuals -> create user session
 
-    if(password === 'test'){
+    if(email === 'user1' && password === 'test'){
         await setUserSession(event, {
             user: {
                 role: 'user'
@@ -13,6 +13,6 @@ export default defineEventHandler(async (event) => {
     }
     throw createError({
         statusCode: 401,
-        message: 'Wrong username or password'
+        message: 'Bad credentials'
     })
 })
