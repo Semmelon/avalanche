3eec6899db36fb801ff244aae4716b43a26b56c4
Semmelon <138894594+Semmelon@users.noreply.github.com>
2025-03-15 14:42:57 +0100

    setup structure + pinia

---
+++ webapp/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { useTestStore } from '~/stores/TestStroe';
+
+    const testStore = useTestStore()
+</script>
+
+<template>
+    This is the homepage: {{ testStore.message }}
+</template>
---
+++ webapp/stores/TestStroe.ts
@@ -0,0 +1,5 @@
+export const useTestStore = defineStore('TestStore', {
+    state: () => ({
+        message: 'this is a test' as string
+      }),
+})
